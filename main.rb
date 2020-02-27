ea4f284bf21480f59c97e5e33f2caa5bc80ae2e0
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-02-27 11:55:56 -0700

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+# a = [1,2,3,4,5,6,7,8]
+
+# puts a
+# print a
+# p a
+
+# p a.last
+
+# puts "What is your first name?"
+# first_name = gets.chomp
+# puts "Thank you, you said your name was #{first_name}"
+# if first_name.length > 5
+# puts "Your full name reversed is #{first_name.reverse}"
+# else
+# puts "The length of your name is #{first_name.length}"
+# end
