79d3413d669b28bedf64e35f2ec059034b37269f
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-02-27 18:18:15 -0700

    Made Crud Module

--- student.rb
+++ student.rb
@@ -1,20 +1,14 @@
-class Student
+require_relative 'crud'
 
+class Student
+    include Crud 
     attr_accessor :first_name, :last_name, :email, :password, :username
     @first_name
     @last_name
     @email
-    @username = "Greggggg"
+    @username
     @password
 
-
-    # def first_name=(name)
-    #     @first_name = name
-    # end
-
-    # def first_name
-    #     @first_name
-    # end 
 
     def initialize(firstname, lastname, username, password, email)
         @first_name = firstname
@@ -31,10 +25,7 @@ end
 
 
 greg = Student.new("Greg", "Anderson", "BabyCakes", "uhoh", "Greg@greg.com")
-puts greg
 lucy = Student.new("lucy", "conk", "conkli", "wooo", "lucy@lucy.com")
-puts lucy
-greg.last_name = lucy.first_name
-puts greg
 
+greg.
 
